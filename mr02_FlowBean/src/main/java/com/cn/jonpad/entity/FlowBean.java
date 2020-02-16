72c58566bdbbe560045f96885faf7483c1b0558c
JonChan <jonchan.hv@outlook.com>
2020-02-16 23:36:11 +0800

    序列化案例实操 - 流量统计

---
+++ mr02_FlowBean/src/main/java/com/cn/jonpad/entity/FlowBean.java
@@ -0,0 +1,92 @@
+package com.cn.jonpad.entity;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+/**
+ * FlowBean 1 实现writable接口
+ */
+public class FlowBean implements Writable {
+
+    /**
+     * 上行流量
+     */
+    private long upFlow;
+    /**
+     * 下行流量
+     */
+    private long downFlow;
+    /**
+     * 总流量
+     */
+    private long sumFlow;
+
+    // 2 反序列化时，需要反射调用空参构造函数，所以必须有
+    public FlowBean() {
+        super();
+    }
+
+    public FlowBean(long upFlow, long downFlow) {
+		super();
+		this.upFlow = upFlow;
+		this.downFlow = downFlow;
+		this.sumFlow = upFlow + downFlow;
+	}
+
+
+    // 3 写序列化方法
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(this.upFlow);
+        out.writeLong(this.downFlow);
+        out.writeLong(this.sumFlow);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        this.upFlow = in.readLong();
+        this.downFlow = in.readLong();
+        this.sumFlow = in.readLong();
+    }
+
+    // 6 编写toString方法，方便后续打印到文本
+	@Override
+	public String toString() {
+		return upFlow + "\t" + downFlow + "\t" + sumFlow;
+	}
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+	public void set(long downFlow, long upFlow) {
+        this.setDownFlow(downFlow);
+        this.setUpFlow(upFlow);
+        this.setSumFlow(downFlow + upFlow);
+	}
+
+    
+}
